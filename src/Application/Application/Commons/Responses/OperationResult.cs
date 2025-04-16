cd36ac2b4ffcb276caa6856cf5046ed82acce36a
mahdiSadeghi6035 <106301977+mahdiSadeghi6035@users.noreply.github.com>
2025-04-16 23:49:34 +0330

    Create operation result

---
+++ src/Application/Application/Commons/Responses/OperationResult.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Commons.Responses;
+
+public struct OperationResult
+{
+    public bool IsSuccess { get; set; }
+    public string[] Messages { get; set; }
+
+    private OperationResult(bool isSuccess, string[] messages)
+    {
+        IsSuccess = isSuccess;
+        Messages = messages;
+    }
+    public static OperationResult Success() => new OperationResult(true, Array.Empty<string>());
+    public static OperationResult Failure(params string[] messages) => new OperationResult(false, messages);
+}
---
+++ test/Application/Application.Test.Unit/Commons/Responses/OperationResultTest.cs
@@ -0,0 +1,36 @@
+﻿using Application.Commons.Responses;
+using FluentAssertions;
+
+namespace Application.Test.Unit.Commons.Responses;
+
+public class OperationResultTest
+{
+
+    [Fact]
+    public void Should_SuccessOperation()
+    {
+        //act
+        OperationResult operationResult = OperationResult.Success();
+
+        //assert
+
+        operationResult.IsSuccess.Should().BeTrue();
+        operationResult.Messages.Should().BeEmpty();
+    }
+
+
+    [Fact]
+    public void Should_FailureOperation()
+    {
+        //arrange
+        string[] messages = new string[] { "Record not found" };
+
+        //act
+        OperationResult result = OperationResult.Failure(messages);
+
+        //assert
+        result.IsSuccess.Should().BeFalse();
+        result.Messages.Should().Equal(messages);
+    }
+
+}
